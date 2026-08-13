a6bfa38a39daf58d1f2dced2cb297296267ed2cd
waziat <mwazy96@gmail.com>
2026-08-13 16:06:22 +0700

    change padding and margin on drawer modal

--- src/components/Modal/ModalContent.tsx
+++ src/components/Modal/ModalContent.tsx
@@ -3,7 +3,7 @@ import type { ModalContentProps } from './Modal.types'
 
 export const ModalContent: React.FC<ModalContentProps> = ({ children, className = '' }) => {
   return (
-    <div className={`flex-1 overflow-y-auto p-6 text-neutral-600 scrollbar-thin scrollbar-thumb-neutral-200 ${className}`}>
+    <div className={`flex-1 overflow-y-auto px-6 py-4 text-neutral-600 scrollbar-thin scrollbar-thumb-neutral-200 ${className}`}>
       {children}
     </div>
   )
